a566f027808073673dfe338b8fc3cf705135b8a8
Bryn Thomas <bryn500@outlook.com>
2019-03-14 15:26:43 +0000

    Docker and KeyVault

--- Queenwood/Program.cs
+++ Queenwood/Program.cs
@@ -5,7 +5,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
 using Microsoft.Extensions.Logging;
 
 namespace Queenwood
@@ -17,8 +20,25 @@ namespace Queenwood
             BuildWebHost(args).Run();
         }
 
+        private const string GetKeyVaultEndpoint = "https://queenwood-secrets.vault.azure.net";
+
         public static IWebHost BuildWebHost(string[] args) =>
                 WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((ctx, builder) =>
+                {
+                    var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+                    var isDevelopment = environment == EnvironmentName.Development;
+
+                    if (!string.IsNullOrEmpty(GetKeyVaultEndpoint) && !isDevelopment)
+                    {
+                        var azureServiceTokenProvider = new AzureServiceTokenProvider();
+                        var keyVaultClient = new KeyVaultClient(
+                            new KeyVaultClient.AuthenticationCallback(
+                                azureServiceTokenProvider.KeyVaultTokenCallback));
+                        builder.AddAzureKeyVault(
+                            GetKeyVaultEndpoint, keyVaultClient, new DefaultKeyVaultSecretManager());
+                    }
+                })
                 .ConfigureKestrel((context, options) =>
                 {
                     options.AddServerHeader = false;
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Queenwood
+
+## Docker
+* docker-compose up -d --build --remove-orphans
+* docker ps --filter "status=running"
+
+### Helpful docker commands
+* docker system prune --volumes
+* docker image ls
+* docker container stop ****
